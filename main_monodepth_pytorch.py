ad0c78ed41fea62938430e8a2e817f9529b6488e
cotus997 <giosue.cotugno@studio.unibo.it>
2022-09-30 13:04:44 +0200

    Update main_monodepth_pytorch.py

--- main_monodepth_pytorch.py
+++ main_monodepth_pytorch.py
@@ -50,7 +50,7 @@ def return_arguments():
                         )
     parser.add_argument('--mode', default='train',
                         help='mode: train or test (default: train)')
-    parser.add_argument('--epochs', default=50,
+    parser.add_argument('--epochs', default=50,type=int,
                         help='number of total epochs to run')
     parser.add_argument('--learning_rate', default=1e-4,
                         help='initial learning rate (default: 1e-4)')
